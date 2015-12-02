1987b8db864cc125a98e0ad9613a685faacceb56
Jai Prakash <jaiprakashgogi@gmail.com>
2015-12-01 23:47:19 -0500

    [Maths][Project] Finding the angle of the face

--- Android/OpenCV Sample - face-detection/src/org/opencv/samples/facedetect/FdActivity.java
+++ Android/OpenCV Sample - face-detection/src/org/opencv/samples/facedetect/FdActivity.java
@@ -12,9 +12,11 @@ import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
+import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
 import org.opencv.objdetect.CascadeClassifier;
@@ -22,232 +24,285 @@ import org.opencv.imgproc.Imgproc;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.WindowManager;
+import android.widget.Button;
 
-public class FdActivity extends Activity implements CvCameraViewListener2 {
+public class FdActivity extends Activity implements CvCameraViewListener2, OnClickListener {
 
-    private static final String    TAG                 = "OCVSample::Activity";
-    private static final Scalar    FACE_RECT_COLOR     = new Scalar(0, 255, 0, 255);
-    public static final int        JAVA_DETECTOR       = 0;
-    public static final int        NATIVE_DETECTOR     = 1;
+	private static final String TAG = "OCVSample::Activity";
+	private static final Scalar FACE_RECT_COLOR = new Scalar(0, 255, 0, 255);
+	public static final int JAVA_DETECTOR = 0;
+	public static final int NATIVE_DETECTOR = 1;
 
-    private MenuItem               mItemFace50;
-    private MenuItem               mItemFace40;
-    private MenuItem               mItemFace30;
-    private MenuItem               mItemFace20;
-    private MenuItem               mItemType;
+	private MenuItem mItemFace50;
+	private MenuItem mItemFace40;
+	private MenuItem mItemFace30;
+	private MenuItem mItemFace20;
+	private MenuItem mItemType;
 
-    private Mat                    mRgba;
-    private Mat                    mGray;
-    private File                   mCascadeFile;
-    private CascadeClassifier      mJavaDetector;
-    private DetectionBasedTracker  mNativeDetector;
+	private Mat mRgba;
+	private Mat mGray;
+	private File mCascadeFile;
+	private CascadeClassifier mJavaDetector;
+	private DetectionBasedTracker mNativeDetector;
 
-    private int                    mDetectorType       = JAVA_DETECTOR;
-    private String[]               mDetectorName;
+	private int mDetectorType = JAVA_DETECTOR;
+	private String[] mDetectorName;
 
-    private float                  mRelativeFaceSize   = 0.2f;
-    private int                    mAbsoluteFaceSize   = 0;
+	private float mRelativeFaceSize = 0.2f;
+	private int mAbsoluteFaceSize = 0;
+	private int imgnum = 1;
 
-    private CameraBridgeViewBase   mOpenCvCameraView;
+	private CameraBridgeViewBase mOpenCvCameraView;
 
-    private BaseLoaderCallback  mLoaderCallback = new BaseLoaderCallback(this) {
-        @Override
-        public void onManagerConnected(int status) {
-            switch (status) {
-                case LoaderCallbackInterface.SUCCESS:
-                {
-                    Log.i(TAG, "OpenCV loaded successfully");
+	private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+		@Override
+		public void onManagerConnected(int status) {
+			switch (status) {
+			case LoaderCallbackInterface.SUCCESS: {
+				Log.i(TAG, "OpenCV loaded successfully");
 
-                    // Load native library after(!) OpenCV initialization
-                    System.loadLibrary("detection_based_tracker");
+				// Load native library after(!) OpenCV initialization
+				System.loadLibrary("detection_based_tracker");
 
-                    try {
-                        // load cascade file from application resources
-                        InputStream is = getResources().openRawResource(R.raw.lbpcascade_frontalface);
-                        File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
-                        mCascadeFile = new File(cascadeDir, "lbpcascade_frontalface.xml");
-                        FileOutputStream os = new FileOutputStream(mCascadeFile);
+				try {
+					// load cascade file from application resources
+					InputStream is = getResources().openRawResource(R.raw.lbpcascade_frontalface);
+					File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+					mCascadeFile = new File(cascadeDir, "lbpcascade_frontalface.xml");
+					FileOutputStream os = new FileOutputStream(mCascadeFile);
 
-                        byte[] buffer = new byte[4096];
-                        int bytesRead;
-                        while ((bytesRead = is.read(buffer)) != -1) {
-                            os.write(buffer, 0, bytesRead);
-                        }
-                        is.close();
-                        os.close();
+					byte[] buffer = new byte[4096];
+					int bytesRead;
+					while ((bytesRead = is.read(buffer)) != -1) {
+						os.write(buffer, 0, bytesRead);
+					}
+					is.close();
+					os.close();
 
-                        mJavaDetector = new CascadeClassifier(mCascadeFile.getAbsolutePath());
-                        if (mJavaDetector.empty()) {
-                            Log.e(TAG, "Failed to load cascade classifier");
-                            mJavaDetector = null;
-                        } else
-                            Log.i(TAG, "Loaded cascade classifier from " + mCascadeFile.getAbsolutePath());
+					mJavaDetector = new CascadeClassifier(mCascadeFile.getAbsolutePath());
+					if (mJavaDetector.empty()) {
+						Log.e(TAG, "Failed to load cascade classifier");
+						mJavaDetector = null;
+					} else
+						Log.i(TAG, "Loaded cascade classifier from " + mCascadeFile.getAbsolutePath());
 
-                        mNativeDetector = new DetectionBasedTracker(mCascadeFile.getAbsolutePath(), 0);
+					mNativeDetector = new DetectionBasedTracker(mCascadeFile.getAbsolutePath(), 0);
 
-                        cascadeDir.delete();
+					cascadeDir.delete();
 
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                        Log.e(TAG, "Failed to load cascade. Exception thrown: " + e);
-                    }
+				} catch (IOException e) {
+					e.printStackTrace();
+					Log.e(TAG, "Failed to load cascade. Exception thrown: " + e);
+				}
 
-                    mOpenCvCameraView.enableView();
-                } break;
-                default:
-                {
-                    super.onManagerConnected(status);
-                } break;
-            }
-        }
-    };
+				mOpenCvCameraView.enableView();
+			}
+				break;
+			default: {
+				super.onManagerConnected(status);
+			}
+				break;
+			}
+		}
+	};
 
-    public FdActivity() {
-        mDetectorName = new String[2];
-        mDetectorName[JAVA_DETECTOR] = "Java";
-        mDetectorName[NATIVE_DETECTOR] = "Native (tracking)";
+	public FdActivity() {
+		mDetectorName = new String[2];
+		mDetectorName[JAVA_DETECTOR] = "Java";
+		mDetectorName[NATIVE_DETECTOR] = "Native (tracking)";
 
-        Log.i(TAG, "Instantiated new " + this.getClass());
-    }
+		Log.i(TAG, "Instantiated new " + this.getClass());
+	}
 
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        Log.i(TAG, "called onCreate");
-        super.onCreate(savedInstanceState);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		Log.i(TAG, "called onCreate");
+		super.onCreate(savedInstanceState);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-        setContentView(R.layout.face_detect_surface_view);
+		setContentView(R.layout.face_detect_surface_view);
 
-        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.fd_activity_surface_view);
-        mOpenCvCameraView.setCameraIndex(1);
-        mOpenCvCameraView.setCvCameraViewListener(this);
-    }
+		mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.fd_activity_surface_view);
+		mOpenCvCameraView.setCameraIndex(1);
+		mOpenCvCameraView.setCvCameraViewListener(this);
+		Button click = (Button) findViewById(R.id.button1);
+		click.setOnClickListener(this);
 
-    @Override
-    public void onPause()
-    {
-        super.onPause();
-        if (mOpenCvCameraView != null)
-            mOpenCvCameraView.disableView();
-    }
+	}
 
-    @Override
-    public void onResume()
-    {
-        super.onResume();
-        if (!OpenCVLoader.initDebug()) {
-            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
-            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
-        } else {
-            Log.d(TAG, "OpenCV library found inside package. Using it!");
-            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
-        }
-    }
+	@Override
+	public void onPause() {
+		super.onPause();
+		if (mOpenCvCameraView != null)
+			mOpenCvCameraView.disableView();
+	}
 
-    public void onDestroy() {
-        super.onDestroy();
-        mOpenCvCameraView.disableView();
-    }
+	@Override
+	public void onResume() {
+		super.onResume();
+		if (!OpenCVLoader.initDebug()) {
+			Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+			OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
+		} else {
+			Log.d(TAG, "OpenCV library found inside package. Using it!");
+			mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+		}
+	}
 
-    public void onCameraViewStarted(int width, int height) {
-        mGray = new Mat();
-        mRgba = new Mat();
-    }
+	public void onDestroy() {
+		super.onDestroy();
+		mOpenCvCameraView.disableView();
+	}
 
-    public void onCameraViewStopped() {
-        mGray.release();
-        mRgba.release();
-    }
+	public void onCameraViewStarted(int width, int height) {
+		mGray = new Mat();
+		mRgba = new Mat();
+	}
 
-    public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+	public void onCameraViewStopped() {
+		mGray.release();
+		mRgba.release();
+	}
 
-        mRgba = inputFrame.rgba();
-        mGray = inputFrame.gray();
-        Core.flip(mRgba,mRgba,0);
-        Core.flip(mGray,mGray,0);
+	public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
 
-        if (mAbsoluteFaceSize == 0) {
-            int height = mGray.rows();
-            if (Math.round(height * mRelativeFaceSize) > 0) {
-                mAbsoluteFaceSize = Math.round(height * mRelativeFaceSize);
-            }
-            mNativeDetector.setMinFaceSize(mAbsoluteFaceSize);
-        }
+		mRgba = inputFrame.rgba();
+		mGray = inputFrame.gray();
+		// Log.i("JAI", "Size is " + mRgba.cols() + "x" + mRgba.rows());
+		/*
+		 * Core.flip(mRgba,mRgba,0); Core.flip(mGray,mGray,0);
+		 */
 
-        MatOfRect faces = new MatOfRect();
+		if (mAbsoluteFaceSize == 0) {
+			int height = mGray.rows();
+			if (Math.round(height * mRelativeFaceSize) > 0) {
+				mAbsoluteFaceSize = Math.round(height * mRelativeFaceSize);
+			}
+			mNativeDetector.setMinFaceSize(mAbsoluteFaceSize);
+		}
 
-        if (mDetectorType == JAVA_DETECTOR) {
-            if (mJavaDetector != null)
-                mJavaDetector.detectMultiScale(mGray, faces, 1.1, 2, 2, // TODO: objdetect.CV_HAAR_SCALE_IMAGE
-                        new Size(mAbsoluteFaceSize, mAbsoluteFaceSize), new Size());
-        }
-        else if (mDetectorType == NATIVE_DETECTOR) {
-            if (mNativeDetector != null)
-                mNativeDetector.detect(mGray, faces);
-        }
-        else {
-            Log.e(TAG, "Detection method is not selected!");
-        }
+		MatOfRect faces = new MatOfRect();
 
-        Rect[] facesArray = faces.toArray();
-        for (int i = 0; i < facesArray.length; i++)
-            Imgproc.rectangle(mRgba, facesArray[i].tl(), facesArray[i].br(), FACE_RECT_COLOR, 3);
+		if (mDetectorType == JAVA_DETECTOR) {
+			if (mJavaDetector != null)
+				mJavaDetector.detectMultiScale(mGray, faces, 1.1, 2, 2, // TODO:
+																		// objdetect.CV_HAAR_SCALE_IMAGE
+						new Size(mAbsoluteFaceSize, mAbsoluteFaceSize), new Size());
+		} else if (mDetectorType == NATIVE_DETECTOR) {
+			if (mNativeDetector != null)
+				mNativeDetector.detect(mGray, faces);
+		} else {
+			Log.e(TAG, "Detection method is not selected!");
+		}
 
-        return mRgba;
-    }
+		double thetaX = 0, thetaY = 0;
+		Rect[] facesArray = faces.toArray();
+		double faceX = mRgba.cols(), faceY = mRgba.rows();
+		int len = facesArray.length > 1 ? 1 : facesArray.length;
+		for (int i = 0; i < len; i++) {
+			Imgproc.rectangle(mRgba, facesArray[i].tl(), facesArray[i].br(), FACE_RECT_COLOR, 3);
+			faceX = facesArray[i].x + facesArray[i].width / 2;
+			faceY = facesArray[i].y + facesArray[i].height / 2;
+			Imgproc.circle(mRgba, new Point(faceX, faceY), 5, new Scalar(255, 255, 0, 255));
+			thetaX = Math.atan((faceX - mRgba.cols()/2) / 1449.3290 ) * 180 / Math.PI;
+			thetaY = Math.atan((faceY - mRgba.rows()/2) / 1449.5544) * 180 / Math.PI;
+		}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        Log.i(TAG, "called onCreateOptionsMenu");
-        mItemFace50 = menu.add("Face size 50%");
-        mItemFace40 = menu.add("Face size 40%");
-        mItemFace30 = menu.add("Face size 30%");
-        mItemFace20 = menu.add("Face size 20%");
-        mItemType   = menu.add(mDetectorName[mDetectorType]);
-        return true;
-    }
+		Imgproc.putText(mRgba, "thetaX: " + thetaX + "thetaY: "+ thetaY, new Point(10, mRgba.rows() - 10), 3, 1,
+				new Scalar(255, 0, 0, 255), 2);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        Log.i(TAG, "called onOptionsItemSelected; selected item: " + item);
-        if (item == mItemFace50)
-            setMinFaceSize(0.5f);
-        else if (item == mItemFace40)
-            setMinFaceSize(0.4f);
-        else if (item == mItemFace30)
-            setMinFaceSize(0.3f);
-        else if (item == mItemFace20)
-            setMinFaceSize(0.2f);
-        else if (item == mItemType) {
-            int tmpDetectorType = (mDetectorType + 1) % mDetectorName.length;
-            item.setTitle(mDetectorName[tmpDetectorType]);
-            setDetectorType(tmpDetectorType);
-        }
-        return true;
-    }
+		return mRgba;
+	}
 
-    private void setMinFaceSize(float faceSize) {
-        mRelativeFaceSize = faceSize;
-        mAbsoluteFaceSize = 0;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		Log.i(TAG, "called onCreateOptionsMenu");
+		mItemFace50 = menu.add("Face size 50%");
+		mItemFace40 = menu.add("Face size 40%");
+		mItemFace30 = menu.add("Face size 30%");
+		mItemFace20 = menu.add("Face size 20%");
+		mItemType = menu.add(mDetectorName[mDetectorType]);
+		return true;
+	}
 
-    private void setDetectorType(int type) {
-        if (mDetectorType != type) {
-            mDetectorType = type;
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		Log.i(TAG, "called onOptionsItemSelected; selected item: " + item);
+		if (item == mItemFace50)
+			setMinFaceSize(0.5f);
+		else if (item == mItemFace40)
+			setMinFaceSize(0.4f);
+		else if (item == mItemFace30)
+			setMinFaceSize(0.3f);
+		else if (item == mItemFace20)
+			setMinFaceSize(0.2f);
+		else if (item == mItemType) {
+			int tmpDetectorType = (mDetectorType + 1) % mDetectorName.length;
+			item.setTitle(mDetectorName[tmpDetectorType]);
+			setDetectorType(tmpDetectorType);
+		}
+		return true;
+	}
 
-            if (type == NATIVE_DETECTOR) {
-                Log.i(TAG, "Detection Based Tracker enabled");
-                mNativeDetector.start();
-            } else {
-                Log.i(TAG, "Cascade detector enabled");
-                mNativeDetector.stop();
-            }
-        }
-    }
+	private void setMinFaceSize(float faceSize) {
+		mRelativeFaceSize = faceSize;
+		mAbsoluteFaceSize = 0;
+	}
+
+	private void setDetectorType(int type) {
+		if (mDetectorType != type) {
+			mDetectorType = type;
+
+			if (type == NATIVE_DETECTOR) {
+				Log.i(TAG, "Detection Based Tracker enabled");
+				mNativeDetector.start();
+			} else {
+				Log.i(TAG, "Cascade detector enabled");
+				mNativeDetector.stop();
+			}
+		}
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case R.id.button1:
+			saveImage(mRgba);
+			break;
+		}
+
+	}
+
+	private void saveImage(Mat img) {
+		// TODO Auto-generated method stub
+		Mat rgb = new Mat();
+		Imgproc.cvtColor(img, rgb, Imgproc.COLOR_BGR2RGB);
+		File path = new File(Environment.getExternalStorageDirectory() + "/opencv/");
+		path.mkdirs();
+		File file = new File(path, "image" + imgnum + ".png");
+		imgnum++;
+		String filename = file.toString();
+		Boolean bool = Imgcodecs.imwrite(filename, rgb);
+		Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+		Uri contentUri = Uri.fromFile(file);
+		mediaScanIntent.setData(contentUri);
+		this.sendBroadcast(mediaScanIntent);
+
+		if (bool)
+			Log.i(TAG, "SUCCESS writing image to external storage");
+		else
+			Log.i(TAG, "Fail writing image to external storage");
+	}
 }
---
+++ matlab/face_detect.m
@@ -0,0 +1,15 @@
+% Create the face detector object.
+faceDetector = vision.CascadeObjectDetector();
+
+% Create the point tracker object.
+pointTracker = vision.PointTracker('MaxBidirectionalError', 2);
+
+% Create the webcam object.
+cam = webcam();
+
+% Capture one frame to get its size.
+videoFrame = snapshot(cam);
+frameSize = size(videoFrame);
+
+% Create the video player object.
+videoPlayer = vision.VideoPlayer('Position', [100 100 [frameSize(2), frameSize(1)]+30]);
